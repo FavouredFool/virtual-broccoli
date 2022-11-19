8d16942f7d4a3b7983e496cdbe7eea6b5f4a8ea7
tistriff <striffler.ti@gmail.com>
2022-11-19 18:41:04 +0100

    updated shader
    fuse different shader usages into one material so only one object with one material with one texture is needed

--- virtual-broccoli/Assets/Recolor.cs
+++ virtual-broccoli/Assets/Recolor.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class Recolor : MonoBehaviour
 {
     [SerializeField]
-    [Range(-1,1)]
+    [Range(-1.5f,1.5f)]
     private float Flowpoint;
     [SerializeField]
     private Material GreyScale;
     [SerializeField]
     private Material NormColor;
 
+    private void Start()
+    {
+        //Minimum of Flowpoint
+    }
+
     void Update()
     {
-        GreyScale.SetFloat("Edge", Flowpoint);
-        NormColor.SetFloat("Edge", Flowpoint);
+        GreyScale.SetFloat("_Edge", Flowpoint);
+        NormColor.SetFloat("_Edge", Flowpoint);
     }
 
 }
