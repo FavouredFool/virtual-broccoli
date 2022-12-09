bdb6e5c095b03ba4c9312f8af83ee85c4a9617c7
Leon Rommel <leon.rommel@student.hs-rm.de>
2022-12-09 16:50:29 +0100

    Darkroom: wheel puzzle

---
+++ virtual-broccoli/Assets/Darkroom_CodePuzzle/Scripts/RotateManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateManager : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _rotatingAxis;
+
+    private List<GameObject> _rotatingWheels;
+    private List<int> _wheelMovements;
+
+    private void Start()
+    {
+        _wheelMovements = new List<int>();
+        _rotatingWheels = new List<GameObject>();
+        if (_rotatingWheels.Count == 0)
+        {
+            GameObject[] foundWheels = GameObject.FindGameObjectsWithTag("Wheel");
+            foreach (GameObject wheel in foundWheels)
+            {
+                Debug.Log("Add " + wheel.name);
+                _rotatingWheels.Add(wheel);
+                _wheelMovements.Add(0);
+            }
+        }
+
+        if (_rotatingWheels.Count != 3)
+        {
+            Debug.Log("Wheels not assigned");
+        }
+    }
+
+    private void Update()
+    {
+        bool reverse = Input.GetKey(KeyCode.LeftShift);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeMovement(0, reverse);
+        }  else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeMovement(1, reverse);
+        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChangeMovement(2, reverse);
+        }
+
+        for (int i = 0; i < _wheelMovements.Count; i++)
+        {
+            int movement = _wheelMovements[i];
+            if (movement != 0)
+            {
+                _rotatingWheels[i].transform.rotation *= Quaternion.Euler(movement * _rotatingAxis);
+                foreach (Transform symbol in _rotatingWheels[i].transform)
+                {
+                    symbol.rotation *= Quaternion.Euler(-movement * _rotatingAxis);
+                }
+            }
+        }
+    }
+
+
+    private void ChangeMovement(int wheelIndex, bool reverse)
+    {
+        int current = _wheelMovements[wheelIndex];
+        int requested = reverse ? -1 : 1;
+        _wheelMovements[wheelIndex] = current == requested ? 0 : requested;
+    }
+}
