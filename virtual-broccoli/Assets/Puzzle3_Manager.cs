422442dcf5f3119b0a04b0f2d2f49626a92c87e9
tistriff <striffler.ti@gmail.com>
2022-12-12 14:23:08 +0100

    implements finish puzzle
    - adding finish puzzle logic and objects
    - creating a parent class for the manager scripts to inherit

--- virtual-broccoli/Assets/Puzzle3_Manager.cs
+++ virtual-broccoli/Assets/Puzzle3_Manager.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Puzzle3_Manager : MonoBehaviour
+public class Puzzle3_Manager : PuzzleTableManager
 {
     [SerializeField]
     private Transform _tower;
-
-    [SerializeField]
-    private Transform _finishBox;
 
     [SerializeField]
     private string[] _nameSeq = new string[4];
@@ -18,55 +15,16 @@ public class Puzzle3_Manager : MonoBehaviour
 
     private Dictionary<string, int> _finishingSequence;
 
-    private enum STATE { AWAITINTERACTION, STARTED, FINISHED };
-    private STATE _gamestate;
-
-    private void Start()
+    protected override void StartExtend()
     {
-        ResetTower();
         _finishingSequence = new Dictionary<string, int>();
-        _finishingSequence.Add(_nameSeq[0], _valueSeq[0]);
-        _finishingSequence.Add(_nameSeq[1], _valueSeq[1]);
-        _finishingSequence.Add(_nameSeq[2], _valueSeq[2]);
-        _finishingSequence.Add(_nameSeq[3], _valueSeq[3]);
-        _gamestate = STATE.AWAITINTERACTION;
-    }
-
-    public void StartGame()
-    {
-        _gamestate = STATE.STARTED;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        switch (_gamestate)
+        for (int i = 0; i < _nameSeq.Length; i++)
         {
-            case STATE.AWAITINTERACTION:
-                Debug.Log("awaiting");
-                if (CheckStarted()) StartGame();
-                return;
-
-            case STATE.STARTED:
-                Debug.Log("started");
-                if (CheckFinished())
-                {
-                    _gamestate = STATE.FINISHED;
-                    FinishGame();
-                }
-                break;
-
-            case STATE.FINISHED:
-                Debug.Log("finished");
-                break;
-
-            default:
-                Debug.Log("error");
-                break;
+            _finishingSequence.Add(_nameSeq[i], _valueSeq[i]);
         }
     }
 
-    private bool CheckStarted()
+    protected override bool CheckStarted()
     {
         foreach (Transform towerChild in _tower)
         {
@@ -75,7 +33,7 @@ public class Puzzle3_Manager : MonoBehaviour
         return false;
     }
 
-    private bool CheckFinished()
+    protected override bool CheckFinished()
     {
         bool state = false;
         foreach (Transform towerChild in _tower)
@@ -86,12 +44,7 @@ public class Puzzle3_Manager : MonoBehaviour
         return state;
     }
 
-    private void FinishGame()
-    {
-        _finishBox.GetChild(0).GetComponent<OpenBoxController>().setOpen(true);
-    }
-
-    private void ResetTower()
+    protected override void ResetComponents()
     {
         foreach (Transform towerChild in _tower)
         {
---
+++ virtual-broccoli/Assets/PuzzleTableManager.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTableManager: MonoBehaviour
+{
+    [SerializeField]
+    protected Transform _finishBox;
+
+    protected enum STATE { AWAITINTERACTION, STARTED, FINISHED };
+    protected STATE _gamestate;
+
+    void Start()
+    {
+        ResetComponents();
+        StartExtend();
+        SwitchState(STATE.AWAITINTERACTION);
+    }
+
+    protected virtual void StartExtend() { }
+
+    void Update()
+    {
+        switch (_gamestate)
+        {
+            case STATE.AWAITINTERACTION:
+                Debug.Log("awaiting");
+                if (CheckStarted()) StartGame();
+                break;
+
+            case STATE.STARTED:
+                Debug.Log("started");
+                GameProgressing();
+                if (CheckFinished())
+                {
+                    FinishGame();
+                    _gamestate = STATE.FINISHED;
+                }
+                break;
+
+            case STATE.FINISHED:
+                Debug.Log("finished");
+                break;
+
+            default:
+                Debug.Log("error");
+                break;
+        }
+        RegularCheck();
+    }
+
+    private void SwitchState(STATE state)
+    {
+        _gamestate = state;
+    }
+
+    protected virtual void ResetComponents() { }
+
+    protected virtual bool CheckStarted()
+    {
+        return false;
+    }
+
+    private void StartGame()
+    {
+        SwitchState(STATE.STARTED);
+    }
+
+    protected virtual void GameProgressing() { }
+
+    protected virtual void RegularCheck() { }
+
+    protected virtual bool CheckFinished()
+    {
+        return false;
+    }
+
+    protected void FinishGame()
+    {
+        _finishBox.GetChild(0).GetComponent<OpenBoxController>().setOpen(true);
+    }
+}
