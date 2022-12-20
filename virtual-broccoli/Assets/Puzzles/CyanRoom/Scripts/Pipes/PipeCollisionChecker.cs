51ba33958b8041cb9abd9e8503b6d2839ffe545f
Leon <leomatrom@gmail.com>
2022-12-20 14:33:57 +0100

    Update room mesh for main area

---
+++ virtual-broccoli/Assets/Puzzles/CyanRoom/Scripts/Pipes/PipeCollisionChecker.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PipeCollisionChecker : MonoBehaviour
+{
+    private Pipe _pipe;
+
+    private void Start()
+    {
+        _pipe = GetComponentInParent<Pipe>();
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (CanEnterOrLeave(collider))
+        {
+            _pipe.AddNeighbor(name, collider.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (CanEnterOrLeave(collider))
+        {
+            _pipe.RemoveNeighbor(name, collider.gameObject);
+        }
+    }
+
+    private bool CanEnterOrLeave(Collider collider)
+    {
+        return collider.gameObject.CompareTag("Ventil") ||
+            ((collider.gameObject.CompareTag("PipeRotateOnly") || collider.gameObject.CompareTag("Pipe"))
+            && (!_pipe.IsDragged() || !collider.gameObject.GetComponent<Pipe>().IsDragged()));
+    }
+
+}
