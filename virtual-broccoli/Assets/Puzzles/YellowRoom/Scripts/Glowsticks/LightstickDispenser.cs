78976edf7bcbc3f18b470f774ed2157392f0803f
Tim Wahrburg <t.wahrburg@gmail.com>
2023-01-10 15:41:57 +0100

    fix: Fixed Dispensor and tarot

--- virtual-broccoli/Assets/Puzzles/YellowRoom/Scripts/Glowsticks/LightstickDispenser.cs
+++ virtual-broccoli/Assets/Puzzles/YellowRoom/Scripts/Glowsticks/LightstickDispenser.cs
@@ -5,7 +5,6 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class LightstickDispenser : XRBaseInteractable
 {
     [SerializeField] private GameObject lightstickPrefab;
-    [SerializeField] private TMP_Text textMesh;
 
     private void Start()
     {
@@ -16,24 +15,15 @@ public class LightstickDispenser : XRBaseInteractable
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
-        int lightCount = int.Parse(textMesh.text);
-        if (lightCount > 0)
-        {
-            lightCount -= 1;
-            CreateAndSelectArrow(args, lightCount);
-            if (lightCount == 0)
-            {
-                GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
-            }
-        }
+
+        CreateAndSelectArrow(args);        
     }
 
-    private void CreateAndSelectArrow(SelectEnterEventArgs args, int lightCount)
+    private void CreateAndSelectArrow(SelectEnterEventArgs args)
     {
         // Create lightstick, force into interacting hand
         OffsetInteractorLightstick lightstick = CreateLightStick(args.interactorObject.transform);
         interactionManager.SelectEnter(args.interactorObject, lightstick);
-        textMesh.text = (lightCount).ToString();
     }
 
     private OffsetInteractorLightstick CreateLightStick(Transform orientation)
