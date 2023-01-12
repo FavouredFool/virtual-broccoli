64ae19256ff4b37f8553f8e63483e13d065aa555
tistriff <striffler.ti@gmail.com>
2023-01-12 19:29:39 +0100

    adjusting mats and finalize soundtrigger to playertrigger with ambient option

--- virtual-broccoli/Assets/SoundTrigger.cs
+++ virtual-broccoli/Assets/SoundTrigger.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class SoundTrigger : MonoBehaviour
 {
     [SerializeField] string name;
+    [SerializeField] bool ambient;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !string.IsNullOrEmpty(name))
         {
             AudioManager.Instance.PlayBackground(name);
+
+            if(ambient) RenderSettings.ambientLight = Color.black;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (ambient) RenderSettings.ambientLight = Color.white;
     }
 }
